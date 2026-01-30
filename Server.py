2369a903e5b1e43da5e1ee81d6aadc993a91127a
UrDeAn <urusovdenis526@gmail.com>
2026-01-30 19:15:25 +0300

    Цену с вб берем(нестабильно конечно)

--- Server.py
+++ Server.py
@@ -1,31 +1,52 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
-import httpx
-from bs4 import BeautifulSoup
 import asyncio
-import requests
+from itertools import product
 
-app = FastAPI()
+from playwright.async_api import async_playwright
 
-# Модель данных, которая приходит от Kotlin
-class Item(BaseModel):
-    url: str
-    id: int
-    name: str
-    price: int
+TARGET_NM = "550070901"
 
-#@app.post("/items/")
-#async def create_item(item: Item):
-    #headers = {'User-Agent': 'Mozilla/5.0'}
-    #async with httpx.AsyncClient() as client:
-        #response = await client.get(item.url, headers=headers)
-    #soup = BeautifulSoup(response.text, 'html.parser')
-    #price_tag = soup.find('ins', class_='priceBlockFinalPrice--iToZR')
-    #if price_tag:
-        #price = price_tag.text.strip()
-    #else:
-        #price = "Цена не найдена"
-    #print(price)
-    #return {"message": "Item received", "item": item}
+async def main():
+    stop_event = asyncio.Event()
 
-#asyncio.run(create_item(Item(url='https://www.wildberries.ru/catalog/489449111/detail.aspx?targetUrl=EX', id=1, name='111', price=1111)))
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(args=["--disable-blink-features=AutomationControlled"])
+        context = await browser.new_context(
+            user_agent=(
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                "AppleWebKit/537.36 (KHTML, like Gecko) "
+                "Chrome/120.0.0.0 Safari/537.36"
+            ),
+            viewport={"width": 1280, "height": 800},
+            locale="ru-RU"
+        )
+        page = await context.new_page()
+
+        async def handle_response(response):
+            if "u-card/cards/v4/detail" in response.url:
+                try:
+                    data = await response.json()
+                    print("JSON ПОЛУЧЕН ✅")
+                    print(int(int(data["products"][0]["sizes"][0]["price"]["product"]) / 100))
+                    stop_event.set()  # сигнализируем main
+                except Exception as e:
+                    print("ERROR", e)
+
+        page.on("response", handle_response)
+
+        await page.goto(
+            f"https://www.wildberries.ru/catalog/{TARGET_NM}/detail.aspx",
+            wait_until="networkidle"
+        )
+
+        # ждём либо 15 секунд, либо пока придёт JSON
+        try:
+            await asyncio.wait_for(stop_event.wait(), timeout=15)
+        except asyncio.TimeoutError:
+            print("JSON не был получен за 15 секунд")
+
+        await browser.close()
+
+asyncio.run(main())
+
+
+#fastapi dev Server.py
