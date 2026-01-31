e863a24f40b522f321ee9e0960564df7f2455794
UrDeAn <urusovdenis526@gmail.com>
2026-01-31 20:28:35 +0300

    Для сервака изменил структуру

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ import asyncio
 from price_wb import wb_price
 import subprocess
 from fastapi import FastAPI
+import os
 articles = [655919326, 550070961, 505535663, 499118925, 608718068]
 TruePrice = [1073, 9277, 1568, 2948, 3704]
 AppPrice = []
@@ -21,10 +22,11 @@ print(AppPrice)
 print(AppPrice == TruePrice) # За 1 день 4 цены уже сменились
 
 app = FastAPI()
+port = int(os.environ.get("PORT", 8000))  # 8000 — значение по умолчанию
 
-@app.get("/item")
+@app.get("/")
 def read_root():
-    return {"message": "Привет! Это простое API на FastAPI."}
+    return {"message": "Hello World"}
 #fastapi dev main.py
 #С ВБ кошельком скидка около 2% (от 2% до 8%)
 # На занятии сделать парсинг артикула с ссылки (веб и из приложения)
