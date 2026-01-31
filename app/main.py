5cfdce74367ca07853834f604eb4c1c39614ade4
UrDeAn <urusovdenis526@gmail.com>
2026-01-31 20:38:51 +0300

    Для сервака изменил структуру

--- app/main.py
+++ app/main.py
@@ -1,25 +1,25 @@
-import asyncio
-from price_wb import wb_price
-import subprocess
+#import asyncio
+#from price_wb import wb_price
+#import subprocess
 from fastapi import FastAPI
 import os
-articles = [655919326, 550070961, 505535663, 499118925, 608718068]
-TruePrice = [1073, 9277, 1568, 2948, 3704]
-AppPrice = []
+#articles = [655919326, 550070961, 505535663, 499118925, 608718068]
+#TruePrice = [1073, 9277, 1568, 2948, 3704]
+#AppPrice = []
 
-try:
-    subprocess.run(['playwright', 'install'], check=True)
-    print("Браузеры успешно установлены.")
-except subprocess.CalledProcessError:
-    print("Ошибка при установке браузеров.")
+#try:
+#    subprocess.run(['playwright', 'install'], check=True)
+#    print("Браузеры успешно установлены.")
+#except subprocess.CalledProcessError:
+#    print("Ошибка при установке браузеров.")
 
-for articl in articles:
-    price = asyncio.run(wb_price(articl))
-    AppPrice.append(price)
+#for articl in articles:
+#    price = asyncio.run(wb_price(articl))
+#    AppPrice.append(price)
 
-print(TruePrice)
-print(AppPrice)
-print(AppPrice == TruePrice) # За 1 день 4 цены уже сменились
+#print(TruePrice)
+#print(AppPrice)
+#print(AppPrice == TruePrice) # За 1 день 4 цены уже сменились
 
 app = FastAPI()
 port = int(os.environ.get("PORT", 8000))  # 8000 — значение по умолчанию
