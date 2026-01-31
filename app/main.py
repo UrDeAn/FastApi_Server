4bb686fe9a87ae303dc3f65e65145eecdfecd936
UrDeAn <urusovdenis526@gmail.com>
2026-01-31 19:36:04 +0300

    Для сервака изменил структуру

--- app/main.py
+++ app/main.py
@@ -1,8 +1,15 @@
 import asyncio
 from price_wb import wb_price
+import subprocess
 articles = [655919326, 550070961, 505535663, 499118925, 608718068]
 TruePrice = [1073, 9277, 1568, 2948, 3704]
 AppPrice = []
+
+try:
+    subprocess.run(['playwright', 'install'], check=True)
+    print("Браузеры успешно установлены.")
+except subprocess.CalledProcessError:
+    print("Ошибка при установке браузеров.")
 
 for articl in articles:
     price = asyncio.run(wb_price(articl))
@@ -10,7 +17,7 @@ for articl in articles:
 
 print(TruePrice)
 print(AppPrice)
-print(AppPrice == TruePrice) # За 1 день 3 цены уже сменились
+print(AppPrice == TruePrice) # За 1 день 4 цены уже сменились
 #fastapi dev main.py
 #С ВБ кошельком скидка около 2% (от 2% до 8%)
 # На занятии сделать парсинг артикула с ссылки (веб и из приложения)
