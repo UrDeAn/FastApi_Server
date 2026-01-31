ff933d74f3500d1bcb059e0a2a4ef11632256eed
UrDeAn <urusovdenis526@gmail.com>
2026-01-31 20:51:42 +0300

    Для сервака изменил структуру

--- app/main.py
+++ app/main.py
@@ -24,9 +24,13 @@ import os
 app = FastAPI()
 port = int(os.environ.get("PORT", 8000))  # 8000 — значение по умолчанию
 
-@app.get("/")
+@app.get("/item")
 def read_root():
     return {"message": "Hello World"}
+
+@app.get("/item/1")
+def read_root():
+    return {"message": "Hello World1"}
 #fastapi dev main.py
 #С ВБ кошельком скидка около 2% (от 2% до 8%)
 # На занятии сделать парсинг артикула с ссылки (веб и из приложения)
